2848c81b938b3e1885ea45d06041c42656dfd424
egehangngrr <45731507+egehangngrr@users.noreply.github.com>
2019-02-02 13:31:49 +0300

    Initial commit

---
+++ WebApplication19/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication19.Models;
+
+namespace WebApplication19.Controllers
+{
+    public class ProductController : ApiController
+    {
+        public IHttpActionResult PostProduct(Product data)
+        {
+            if (ModelState.IsValid)
+            {
+                return Ok(data);
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
---
+++ WebApplication19/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication19.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        [Range(0,999)]
+        public double Weight { get; set; }
+    }
+}
